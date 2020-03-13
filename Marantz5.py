09740dd57e6412da4244be6e1f68b8b739c085c3
tobiz <pjrobinson@metronet.co.uk>
2020-03-13 17:50:17 +0000

    Minor mods

--- Marantz5.py
+++ Marantz5.py
@@ -21,8 +21,6 @@ class IP():
     # It might be possible to use the following rather than netcat - for later!
     #
         
-    
-
     def netcat(hostname, port, content):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((hostname, port))
